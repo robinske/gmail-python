98d698b0d09a0c28ccdc33c008d17a5ebc1dff83
Kelley Robinson <krobinson@twilio.com>
2018-03-01 16:37:51 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Sending Email with Gmail SMTP, Python and Flask
+
+This is a sample project that will allow you to send email with gmail's SMTP server, python, and flask.
+
+Blog post coming soon!
---
+++ send.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_mail import Mail, Message
+import os
+
+app = Flask(__name__)
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
+if __name__ == '__main__':
+    with app.app_context():
+        msg = Message(subject="Hello",
+                      sender=app.config.get("MAIL_USERNAME"),
+                      recipients=["<recipient email here>"], # use your email for testing
+                      body="This is a test email I sent with Gmail and Python!")
+        mail.send(msg)
